d890829c3037675f211829fa3363bdd3068f5cdb
Kadek Chresna <chresna@leson.id>
2020-06-09 02:01:42 +0700

    Fixing error template

--- rest_errors/errors.go
+++ rest_errors/errors.go
@@ -2,61 +2,89 @@ package errors
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 )
 
-type ResErr struct {
-	Message string        `json:"message"`
-	Status  int           `json:"status"`
-	Error   string        `json:"error"`
-	Causes  []interface{} `json:"causes"`
+type resErr struct {
+	ErrMessage string        `json:"message"`
+	ErrStatus  int           `json:"status"`
+	ErrError   string        `json:"error"`
+	ErrCauses  []interface{} `json:"causes"`
+}
+
+type ResErr interface {
+	Message() string
+	Status() int
+	Error() string
+	Causes() []interface{}
+}
+
+func (e resErr) Error() string {
+	return fmt.Sprintf("message: %s, status: %d, error: %s, causes [ %v ]", e.ErrMessage, e.ErrStatus, e.ErrError, e.ErrCauses)
+}
+
+func (e resErr) Message() string {
+	return e.ErrMessage
+}
+
+func (e resErr) Status() int {
+	return e.ErrStatus
+}
+
+func (e resErr) Causes() []interface{} {
+	return e.ErrCauses
+}
+
+func NewError(message string) error {
+	return errors.New(message)
 }
 
 // NewBadRequestError for error template with status 400
-func NewBadRequestError(message string) *ResErr {
-	return &ResErr{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "bad_request",
+func NewBadRequestError(message string) ResErr {
+	return resErr{
+		ErrMessage: message,
+		ErrStatus:  http.StatusBadRequest,
+		ErrError:   "bad_request",
 	}
 
 }
 
-func NewRestError(message string, status int, err string, causes []interface{}) *ResErr {
-	return &ResErr{
-		Message: message,
-		Status:  status,
-		Error:   "bad_request",
-		Causes:  causes,
+func NewRestError(message string, status int, err string, causes []interface{}) ResErr {
+	return resErr{
+		ErrMessage: message,
+		ErrStatus:  status,
+		ErrError:   "bad_request",
+		ErrCauses:  causes,
 	}
 }
 
-func NewUnAuthorizedError() *ResErr {
-	return &ResErr{
-		Message: "access token is not valid",
-		Status:  http.StatusUnauthorized,
-		Error:   "unauthorized",
+func NewUnAuthorizedError() ResErr {
+	return resErr{
+		ErrMessage: "access token is not valid",
+		ErrStatus:  http.StatusUnauthorized,
+		ErrError:   "unauthorized",
 	}
 }
 
 // NewNotFoundError for error template with status 40
-func NewNotFoundError(message string) *ResErr {
-	return &ResErr{
-		Message: message,
-		Status:  http.StatusNotFound,
-		Error:   "not_found",
+func NewNotFoundError(message string) ResErr {
+	return resErr{
+		ErrMessage: message,
+		ErrStatus:  http.StatusNotFound,
+		ErrError:   "not_found",
 	}
 }
 
 // NewInternalServerError something went wrong
-func NewInternalServerError(message string, err error) *ResErr {
-	if err == nil {
-		err = errors.New("something when wrong i guess")
+func NewInternalServerError(message string, err error) ResErr {
+	result := resErr{
+		ErrMessage: message,
+		ErrStatus:  http.StatusInternalServerError,
+		ErrError:   "internal_server_error",
 	}
-	return &ResErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "internal_server_error",
-		Causes:  []interface{}{err},
+	if err != nil {
+		result.ErrCauses = append(result.ErrCauses, err.Error())
 	}
+	return result
 }
--- rest_errors/errors_test.go
+++ rest_errors/errors_test.go
@@ -2,6 +2,7 @@ package errors
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -11,34 +12,35 @@ import (
 func TestNewInternalError(t *testing.T) {
 	err := NewInternalServerError("this is message", errors.New("database error"))
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "this is message", err.Message)
-	assert.EqualValues(t, "internal_server_error", err.Error)
+	fmt.Printf("%#v", err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
+	assert.EqualValues(t, "this is message", err.Message())
+	assert.EqualValues(t, "internal_server_error", err.Error())
 
 	assert.NotNil(t, err.Causes)
-	assert.EqualValues(t, 1, len(err.Causes))
-	assert.EqualValues(t, errors.New("database error"), err.Causes[0])
+	assert.EqualValues(t, 1, len(err.Causes()))
+	assert.EqualValues(t, errors.New("database error"), err.Causes()[0])
 
 	err = NewInternalServerError("this is message", nil)
-	assert.EqualValues(t, errors.New("something when wrong i guess"), err.Causes[0])
+	assert.EqualValues(t, errors.New("something when wrong i guess"), err.Causes()[0])
 }
 
 func TestNewBadRequestError(t *testing.T) {
 	// TODO
 	err := NewNotFoundError("this is message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusNotFound, err.Status)
-	assert.EqualValues(t, "this is message", err.Message)
-	assert.EqualValues(t, "not_found", err.Error)
+	assert.EqualValues(t, http.StatusNotFound, err.Status())
+	assert.EqualValues(t, "this is message", err.Message())
+	assert.EqualValues(t, "not_found", err.Error())
 }
 
 func TestNewNotFoundError(t *testing.T) {
 	// TODO
 	err := NewBadRequestError("this is message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusBadRequest, err.Status)
-	assert.EqualValues(t, "this is message", err.Message)
-	assert.EqualValues(t, "bad_request", err.Error)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+	assert.EqualValues(t, "this is message", err.Message())
+	assert.EqualValues(t, "bad_request", err.Error())
 }
 
 func TestNewError(t *testing.T) {
